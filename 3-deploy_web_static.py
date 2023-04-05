e3cf93cf72f7829c40be0e88e1ea2b39a02052fb
Emenike Nathaniel <emenikenathaniel55@gmail.com>
2023-04-05 19:10:06 +0000

    AirBnB clone - Deploy static

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from fabric.api import env, local, put, run, runs_once
 
 
-env.hosts = ["34.73.0.174", "35.196.78.105"]
+env.hosts = ["18.206.197.218", "52.86.249.195"]
 """The list of host server IP addresses."""
 
 
